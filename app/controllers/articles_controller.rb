d5516a6a46ca2b77551559c0bed5b13279515629
Paulina Osiak <paulina.osiak@gmail.com>
2017-06-02 15:04:13 +0200

    articles with a few actions

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,55 @@
+class ArticlesController < ApplicationController
+
+	def index
+		@articles = Article.all
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			flash.now[:notice] = "Success"
+			redirect_to articles_path
+		else
+			flash[:error] = "Something was wrong"
+			render :new
+		end
+	end
+
+	# def edit
+	# 	@article = Article.find(params[:id])
+	# end
+
+	# def update
+	# 	@article = Article.find(params[:id])
+	# 	if @article.update(article_params)
+	# 		flash[:notice] = "Your post was updated"
+	# 		redirect_to articles_path
+	# 	else
+	# 		flash[:error] = "Nie udało się zapisać zmian"
+	# 		render :edit
+	# 	end
+	# end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		flash[:notice] = "Your post was deleted"
+		redirect_to articles_path
+	end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :body)
+	end
+
+
+end
